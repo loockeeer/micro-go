57886ff43eafde8270b979eb4a218e69a8276c53
loockeeer <lulu.r1313@gmail.com>
2025-12-02 11:42:55 +0100

    feat: add nix package

--- flake.nix
+++ flake.nix
@@ -1,24 +1,38 @@
 {
-	description = "Projet de compilation";
+  description = "Projet de compilation";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+  };
 
-	outputs = { self, nixpkgs }:
-	let
-		system = "x86_64-linux";
-		pkgs = import nixpkgs { system = system; };
-	in
-	{
-		devShells.${system}.default = pkgs.mkShell {
-			packages = with pkgs; [
-				ocaml
-				dune_2
-				ocamlPackages.findlib
-				ocamlformat
-			];
-		};
-	};
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { system = system; };
+    in
+    {
+      formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-tree;
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          ocaml
+          dune_2
+          ocamlPackages.findlib
+          ocamlformat
+        ];
+      };
+      packages.${system}.default = pkgs.ocamlPackages.buildDunePackage {
+        pname = "micro-go";
+        version = "1.0.0";
+
+        minimalOCamlVersion = "4.02";
+
+        src = ./.;
+
+        meta = {
+          homepage = "https://github.com/loockeeer/micro-go";
+          description = "Class project";
+        };
+      };
+    };
 }
-
